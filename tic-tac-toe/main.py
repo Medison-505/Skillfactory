1a917ed7db220223e4facf6f99d0f4af188bfb3b
Alena Skobeleva <askobeleva@pnblab.com>
2021-04-11 12:43:06 +0300

    update tic-tac-toe

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,72 +1,102 @@
 # Я на 120% уверена, что этот код не будут читать люди, которые не говорят на русском языке.
 
-# Создаем игровое поле
-print('Это игра крестики-нолики. \nЧтобы сделать ход укажите координаты ячейки: номер строки затем номер столбца. '
-      '\nНапример, если вы хотите пойти в левый верхний угол введите 00')
-print()
-
-game_board = [[' ', '0', '1', '2'],
-              ['0', '-', '-', '-'],
-              ['1', '-', '-', '-'],
-              ['2', '-', '-', '-']]
-
-for row in game_board:
-    for col in row:
-        print(col, end=' ')
+# Players greeting
+def greeting():
+    print('Это игра крестики-нолики.')
+    print('Чтобы сделать ход укажите координаты ячейки: номер строки пробел номер столбца.')
+    print('Например, если вы хотите пойти в левый верхний угол введите "0 0".')
     print()
 
 
-# Приглашаем игрока ко вводу и устанавливаем координаты хода
-
-def player_move_func(player_move, team):
-    print()
-    player_move = list(map(int, input('Вы играете за игрока ' + team + '. Введите координаты своего хода.')))
-    print()
-    for i in range(len(game_board)):
-        for j in range(len(game_board[i])):
-            game_board[player_move[0] + 1][player_move[1] + 1] = team
-            print(game_board[i][j], end=' ')
-        print()
+# Create a game board
+field = [[" "] * 3 for i in range(3)]
 
 
-# Цикл ходов
+# print a game board
+def show_game_board():
+    print(f'  0 1 2')
+    for i in range(3):
+        row = ' '.join(field[i])
+        print(f'{i} {row}')
+
+
+# Ask coordinates
+def ask_coordinates():
+    while True:
+        coordinates = input('Введите координаты: ').split()
+
+        if len(coordinates) != 2:
+            print('Ведите две координаты.')
+            continue
+
+        x, y = coordinates
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print('Введите числовые координаты.')
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print('Координаты вне диапазона.')
+            continue
+
+        if field[x][y] != " ":
+            print('Клетка уже занята.')
+            continue
+
+        return x, y
+
+
+# Check winner
+def check_win():
+    win_coordinates = (
+        ((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+        ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)),
+        ((0, 0), (1, 0), (2, 0)), ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2))
+    )
+
+    for coordinates in win_coordinates:
+        symbols = []
+        for c in coordinates:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X!!!")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0!!!")
+            return True
+
+    return False
+
+
+# Moving loop
+greeting()
+
 move_count = 0
 
-player_move_func('x_player_move', 'X')
+while True:
+    move_count += 1
 
-while move_count < 8:
-    player_move_func('o_player_move', '0')
+    show_game_board()
 
-    if (game_board[1][1] == '0' and game_board[2][2] == '0' and game_board[3][3] == '0' or
-            game_board[1][3] == '0' and game_board[2][2] == '0' and game_board[3][1] == '0' or
-            game_board[1][1] == '0' and game_board[1][2] == '0' and game_board[1][3] == '0' or
-            game_board[2][1] == '0' and game_board[2][2] == '0' and game_board[2][3] == '0' or
-            game_board[3][1] == '0' and game_board[3][2] == '0' and game_board[3][3] == '0' or
-            game_board[1][1] == '0' and game_board[2][1] == '0' and game_board[3][1] == '0' or
-            game_board[1][2] == '0' and game_board[2][2] == '0' and game_board[3][2] == '0' or
-            game_board[1][3] == '0' and game_board[2][3] == '0' and game_board[3][3] == '0'):
-        print('Игрок 0 победил!')
+    if move_count % 2 == 1:
+        print('Ходит крестик!')
+    else:
+        print('Ходит нолик!')
+
+    x, y = ask_coordinates()
+
+    if move_count % 2 == 1:
+        field[x][y] = 'X'
+    else:
+        field[x][y] = '0'
+
+    if check_win():
         break
 
-    player_move_func('x_player_move', 'X')
-
-    if (game_board[1][1] == 'X' and game_board[2][2] == 'X' and game_board[3][3] == 'X' or
-            game_board[1][3] == 'X' and game_board[2][2] == 'X' and game_board[3][1] == 'X' or
-            game_board[1][1] == 'X' and game_board[1][2] == 'X' and game_board[1][3] == 'X' or
-            game_board[2][1] == 'X' and game_board[2][2] == 'X' and game_board[2][3] == 'X' or
-            game_board[3][1] == 'X' and game_board[3][2] == 'X' and game_board[3][3] == 'X' or
-            game_board[1][1] == 'X' and game_board[2][1] == 'X' and game_board[3][1] == 'X' or
-            game_board[1][2] == 'X' and game_board[2][2] == 'X' and game_board[3][2] == 'X' or
-            game_board[1][3] == 'X' and game_board[2][3] == 'X' and game_board[3][3] == 'X'):
-        print('Игрок Х победил!')
+    if move_count == 9:
+        print('Ходов больше нет. Ничья.')
         break
-
-    move_count += 2
-else:
-    print()
-    print('Ходов больше нет.')
 
 print('Игра окончена.')
-
-
-
